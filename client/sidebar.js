6e9d70747e795960171ac99cde972aac76ec3e50
ltaht <specter15564@gmail.com>
2025-01-14 03:02:48 +0700

    add sidebar project and board section with styling

--- client/sidebar.js
+++ client/sidebar.js
@@ -1,15 +1,17 @@
 export function Sidebar(user) {
-    return `
-      <div id="sidebar">
-        <div id="logo">
-        </div>
-        <div id="userbox">
-          <img src="${user.avatarUrl}" alt="Profile Picture" id="profile-pic">
-          <span id="username">${user.username}</span>
-        </div>
-        <div id="projectbox">
-        </div>
+  return `
+    <div id="sidebar">
+      <div id="logo"></div>
+      <div id="userbox">
+        <img src="${user.avatarUrl}" alt="Profile Picture" id="profile-pic">
+        <span id="username">${user.username}</span>
       </div>
-    `;
-  }
-  
+      <div id="projectbox" class="clickable">Projects</div>
+      <div id="board-section">
+        <span id="board-text">Boards</span>
+        <button id="create-board-button">+ Create New Board</button>
+      </div>
+
+    </div>
+  `;
+}
