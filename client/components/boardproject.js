a0d31084ff504ed0854c909fc7a6d16c04316012
ltaht <specter15564@gmail.com>
2025-01-24 18:17:26 +0700

    feat: add triangle symbol to project box; update project name and description display

--- client/components/boardproject.js
+++ client/components/boardproject.js
@@ -1,17 +1,25 @@
 export function BoardProject() {
+  const projectBox = document.getElementById("projectbox");
+  const boardSection = document.getElementById("board-section");
+  const projectSection = document.getElementById("project-section");
+  const triangleSymbol = document.createElement("div");
 
-const projectBox = document.getElementById("projectbox");
-const boardSection = document.getElementById("board-section");
-const projectSection = document.getElementById("project-section");
+  // Add triangle symbol to projectBox
+  triangleSymbol.id = "triangle-symbol";
+  triangleSymbol.classList.add("triangle-right"); // Default direction
+  projectBox.prepend(triangleSymbol);
 
-projectBox.addEventListener("click", () => {
-  if (boardSection.style.display === "none") {
-    boardSection.style.display = "block";
-    projectSection.style.display = "none";
-  } else {
-    boardSection.style.display = "none";
-    projectSection.style.display = "block";
-  }
-});
-
+  projectBox.addEventListener("click", () => {
+    if (boardSection.style.display === "none") {
+      boardSection.style.display = "block";
+      projectSection.style.display = "none";
+      triangleSymbol.classList.remove("triangle-down");
+      triangleSymbol.classList.add("triangle-right");
+    } else {
+      boardSection.style.display = "none";
+      projectSection.style.display = "block";
+      triangleSymbol.classList.remove("triangle-right");
+      triangleSymbol.classList.add("triangle-down");
+    }
+  });
 }
